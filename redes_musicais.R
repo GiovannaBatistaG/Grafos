6534409cf43fc41216e1b460ed977c0e6fcd49bd
GiovannaBatistaG <giovanna.batis123@gmail.com>
2025-12-01 20:15:00 -0300

    Add files via upload

---
+++ redes_musicais.R
@@ -0,0 +1,6 @@
+if(!requireNamespace("dplyr", quietly=TRUE)) install.packages("dplyr")
+if(!requireNamespace("tidyr", quietly=TRUE)) install.packages("tidyr")
+if(!requireNamespace("igraph", quietly=TRUE)) install.packages("igraph")
+if(!requireNamespace("proxy", quietly=TRUE)) install.packages("proxy")      # para similaridade Jaccard
+if(!requireNamespace("openxlsx", quietly=TRUE)) install.packages("openxlsx")# para salvar Excel
+
